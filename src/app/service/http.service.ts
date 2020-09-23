86d3f7472a489880f7f1cd20beacbfcd1cc7c4bb
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-09-23 09:06:19 +0530

    Modify: method name

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -9,7 +9,7 @@ export class HttpService {
   constructor(private http: HttpClient) { }
   url = "https://quze-intern-test.s3.us-east-2.amazonaws.com/course-data.json"
 
-  post() {
+  get() {
     return this.http.get(this.url)
   }
 }
