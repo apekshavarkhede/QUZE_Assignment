4909b00b7590fa6e104dddc49fab76f3963a6391
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-09-23 09:09:35 +0530

    Add: method to get data using http

--- src/app/components/courses-name/courses-name.component.ts
+++ src/app/components/courses-name/courses-name.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/service/http.service';
 
 @Component({
   selector: 'app-courses-name',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses-name.component.css']
 })
 export class CoursesNameComponent implements OnInit {
-  
-  constructor() { }
-  value=""
+
+  constructor(private http: HttpService) { }
+  value = "";
+  coursesName: Array<any>
+
+  getData() {
+    this.http.get().subscribe(data => {
+      let completeData = Object.entries(data);
+      this.coursesName = [];
+      completeData.forEach((item) => {
+        this.coursesName.push(item[1].title)
+      })
+    })
+  }
+
   ngOnInit() {
+    this.getData()
   }
 
 }
