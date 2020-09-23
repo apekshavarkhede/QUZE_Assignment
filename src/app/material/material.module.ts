e131f66fb9c762baef3c9e79b6df52a2c9827309
apekshavarkhede <apekshavarkhede1508@gmail.com>
2020-09-23 11:51:19 +0530

    Add: angular material imports

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ],
   exports:[
-    MatToolbarModule
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
